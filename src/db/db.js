15f6e386c9e7a81903163bd79e9eb33fc5a3bb3e
ashmit <ashmitbastola@gmail.com>
2025-01-19 19:43:20 +0545

    sequalize initialized

---
+++ src/db/db.js
@@ -0,0 +1,3 @@
+import { Sequelize } from "sequelize";
+
+export const sequalize=new Sequelize('connection string')
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { configDotenv } from "dotenv";
 import express from "express";
+// import { sequalize } from "./db/db";
 configDotenv()
-const app=new express()
-const port=3000 || process.env.PORT
-
-app.listen(port,async()=>{
-    console.log(`Server is listening on port ${port}`);
+const app = new express()
+const port = 3000 || process.env.PORT
+// await sequalize.authenticate().then(()=>console.log("Database connection established...")).catch((err)=>console.log("Failed to connect to database ",err))
+app.listen(port, async () => {
+console.log(`Server is listening on port ${port}`);
 })
