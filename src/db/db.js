220ff315a5a324057b01b36690c8483eab00899b
ashmit <ashmitbastola@gmail.com>
2025-01-19 20:37:42 +0545

    test

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,4 @@
 import { configDotenv } from "dotenv";
 import { Sequelize } from "sequelize";
 configDotenv()
-export const sequelize = new Sequelize(process.env.DATABASE_CONNECTION_URI);
+export const sequelize = new Sequelize(process.env.DATABASE_CONNECTION_URI)
