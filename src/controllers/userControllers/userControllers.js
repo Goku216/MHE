148e9c078fdc47b62e8089e74312b8e7beff9ac4
ashmit <ashmitbastola@gmail.com>
2025-01-19 21:39:43 +0545

    json update

--- src/controllers/userControllers/userControllers.js
+++ src/controllers/userControllers/userControllers.js
@@ -2,13 +2,29 @@ export async function userRegister(req,res) {
     try {
         const {username,email,password,isAdmin}=req.body
         console.log(username,email,password,isAdmin);
+       if (!username) {
         return res.json({
-            username,
-            email,
-            password,
-            isAdmin
+            message:"Username is required!",
         })
-        console.log(username,email,password,isAdmin);
+       }
+       if (!email) {
+        return res.json({
+            message:"Email is required!",
+        })
+       }
+       if (!password) {
+        return res.json({
+            message:"Password is required!",
+        })
+       }
+
+       return res.json({
+        username,
+        email,
+        password,
+        isAdmin
+       })
+
     } catch (error) {
         return res.json({
             message:error.message
