972c31464828cd770e742341daa591ef6eb97994
ashmit <ashmitbastola@gmail.com>
2025-01-19 22:31:43 +0545

    database update

--- src/controllers/userControllers/userControllers.js
+++ src/controllers/userControllers/userControllers.js
@@ -43,3 +43,55 @@ export async function userRegister(req,res) {
         })
     }
 }
+
+
+export async function userLogin(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({
+                message: "Email is required!",
+            });
+        }
+
+        if (!password) {
+            return res.status(400).json({
+                message: "Password is required!",
+            });
+        }
+
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found!",
+            });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(401).json({
+                message: "Invalid Password",
+            });
+        }
+
+        const isAdmin = user.isAdmin;
+
+        const token = jwt.sign(
+            { userId: user.id, isAdmin },
+            isAdmin ? process.env.JWT_SECRET_ADMIN : process.env.JWT_SECRET_USER,
+            { expiresIn: "1h" } 
+        );
+
+        return res.json({
+            token,
+            isAdmin
+        });
+
+    } catch (error) {
+        return res.json({
+            message: error.message
+        });
+    }
+}
+
--- src/routes/userRoutes/userRoutes.js
+++ src/routes/userRoutes/userRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { userRegister } from "../../controllers/userControllers/userControllers.js";
+import { userLogin, userRegister } from "../../controllers/userControllers/userControllers.js";
 
 export const userRouter=Router()
 
-userRouter.post('/user',userRegister)
+userRouter.post('/user/register',userRegister)
+userRouter.post('/user/login',userLogin)
