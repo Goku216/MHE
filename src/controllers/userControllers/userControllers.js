53442cb2e72e5d73d38b41f819e064c9c46cb955
ashmit <ashmitbastola@gmail.com>
2025-01-19 21:42:46 +0545

    status code update

--- src/controllers/userControllers/userControllers.js
+++ src/controllers/userControllers/userControllers.js
@@ -3,22 +3,22 @@ export async function userRegister(req,res) {
         const {username,email,password,isAdmin}=req.body
         console.log(username,email,password,isAdmin);
        if (!username) {
-        return res.json({
+        return res.status(400).json({
             message:"Username is required!",
         })
        }
        if (!email) {
-        return res.json({
+        return res.status(400).json({
             message:"Email is required!",
         })
        }
        if (!password) {
-        return res.json({
+        return res.status(400).json({
             message:"Password is required!",
         })
        }
 
-       return res.json({
+       return res.status(400).json({
         username,
         email,
         password,
